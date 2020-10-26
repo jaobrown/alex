cb6ff3f573a3f1a86ad20dbc9afd617233f28b46
Jared Brown <jared.brown@elementthree.com>
2020-10-26 18:33:03 -0400

    Implement lots of stuff tbh

--- src/components/globals/Seo/Seo.jsx
+++ src/components/globals/Seo/Seo.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React from 'react'
 
 export const Seo = () => {
   return <div className="">Seo</div>
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,7 @@
 import React from 'react'
 
 const NotFoundPage = () => {
-    return (
-        <div>
-            Haha, oops
-        </div>
-    )
+  return <div>Haha, oops</div>
 }
 
 export default NotFoundPage
